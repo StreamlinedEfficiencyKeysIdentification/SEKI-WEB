a24b007f18aa321996f1e494eea3e2148a993cbe
Iago de Souza Lopes <iagodesouza2012@gmail.com>
2024-08-27 16:48:45 -0300

    Criando e estilizando a página Home

---
+++ src/Pages/Home/Home.jsx
@@ -0,0 +1,12 @@
+/* eslint-disable react/react-in-jsx-scope */
+import './home.css';
+
+function Home() {
+  return (
+    <section id="home">
+      <div className="container"></div>
+    </section>
+  );
+}
+
+export default Home;

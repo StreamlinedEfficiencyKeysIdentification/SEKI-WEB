fb11d3db17e26afaf040320a30018ba6e54b155b
Iago de Souza Lopes <iagodesouza2012@gmail.com>
2024-08-28 17:44:41 -0300

    Preparando novos arquivos de páginas

---
+++ src/Pages/QuemSomos/Sobre.jsx
@@ -0,0 +1,12 @@
+/* eslint-disable react/react-in-jsx-scope */
+import './sobre.css';
+
+function Sobre() {
+  return (
+    <section id="Sobre">
+      <h1>teste</h1>
+    </section>
+  );
+}
+
+export default Sobre;

811b684a55da69da30ee799695bf1d8190abcac1
Iago de Souza Lopes <iagodesouza2012@gmail.com>
2024-08-28 17:43:48 -0300

    Adicionando novas páginas

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,16 @@ import './assets/global.css';
 import Header from './Components/Header/Header';
 import Footer from './Components/Footer/Footer';
 import Home from './Pages/Home/Home';
+import Controle from './Pages/Controle/Controle';
+import Sobre from './Pages/QuemSomos/Sobre';
 
 function App() {
   return (
     <div>
       <Header />
       <Home />
+      <Controle />
+      <Sobre />
       <Footer />
     </div>
   );

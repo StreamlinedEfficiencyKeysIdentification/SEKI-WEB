a7a002a93f9e163de268f25e79fe96758c067e8e
Iago de Souza Lopes <iagodesouza2012@gmail.com>
2024-09-13 13:28:59 -0300

    Finalização da estrutura e estilização da página de recursos

--- src/Pages/Recursos/Recurso.jsx
+++ src/Pages/Recursos/Recurso.jsx
@@ -5,8 +5,23 @@ import QrCode from '/QrCode.svg';
 import Arrow from '/Arrow.svg';
 import RecursoImg from '/Recurso.svg';
 import './recurso.css';
+import { useEffect } from 'react';
 
 function Recurso() {
+  useEffect(() => {
+    document.querySelectorAll('details').forEach((faq) => {
+      faq.addEventListener('toggle', () => {
+        const icon = faq.querySelector('.icon');
+
+        if (faq.open) {
+          icon.textContent = '-';
+        } else {
+          icon.textContent = '+';
+        }
+      });
+    });
+  }, []);
+
   return (
     <section className="section">
       <div className="app">
@@ -39,12 +54,94 @@ function Recurso() {
         </div>
       </div>
       <div id="Recurso">
+        <div className="rc-titulo">
+          <p>Nossos principais recursos</p>
+        </div>
         <div className="r-container">
-          <div className="rc-titulo">
-            <p>Nossos principais recursos</p>
+          <div className="left-column">
+            <div className="resource-box">
+              <h2>Controle de patrimônio</h2>
+              <p>Gerencie todos os patrimônios em um só lugar.</p>
+            </div>
+            <div className="resource-box">
+              <h2>Abertura de chamados</h2>
+              <p>Facilite a resolução de problemas técnicos na sua empresa.</p>
+            </div>
+            <div className="resource-box">
+              <h2>Rastreabilidade</h2>
+              <p>Tenha, ao seu alcance, os detalhes e os locais onde se encontram seus bens patrimoniais.</p>
+            </div>
           </div>
+
           <div className="recurso">
             <img src={RecursoImg} alt="" />
+            <div className="sombra-img"></div>
+          </div>
+
+          <div className="right-column">
+            <div className="resource-box">
+              <h2>Criação de Filiais</h2>
+              <p>Tenha o controle de suas filiais na palma da sua mão.</p>
+            </div>
+            <div className="resource-box">
+              <h2>Relatórios</h2>
+              <p>Resumos e resultados incríveis, com gráficos simples e completos.</p>
+            </div>
+            <div className="resource-box">
+              <h2>Gestão Eficiente dos Seus Bens</h2>
+              <p>Mantenha um controle preciso sobre seus bens.</p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="perguntas">
+        <div className="rc-titulo">
+          <p>Perguntas frequentes</p>
+        </div>
+
+        <div className="container-perguntas">
+          <div className="faq">
+            <details className="faq-item">
+              <summary>
+                <span>Qual a importância do controle patrimonial para uma empresa?</span>
+                <span className="icon">+</span>
+              </summary>
+              <p>
+                O controle patrimonial permite que a empresa gerencie seus bens de forma eficiente, evitando perdas e
+                facilitando a manutenção dos ativos.
+              </p>
+            </details>
+
+            <details className="faq-item">
+              <summary>
+                <span>Vou poder utilizar no celular também?</span>
+                <span className="icon">+</span>
+              </summary>
+              <p>Sim, o sistema SEKI é compatível com dispositivos móveis, permitindo o uso no celular ou tablet.</p>
+            </details>
+
+            <details className="faq-item">
+              <summary>
+                <span>O SEKI pode ser usado offline?</span>
+                <span className="icon">+</span>
+              </summary>
+              <p>
+                Sim, o SEKI pode ser usado offline com recursos limitados. Algumas funções dependem de conexão com a
+                internet.
+              </p>
+            </details>
+          </div>
+
+          <div className="contact-section">
+            <div className="contato-info">
+              <h2>Ainda com dúvidas?</h2>
+              <p>
+                Caso tenha ficado com alguma dúvida sobre o uso do app, entre em contato através do e-mail para você
+                aproveitar o SEKI ao máximo!
+              </p>
+            </div>
+            <button>Entre em contato</button>
           </div>
         </div>
       </div>

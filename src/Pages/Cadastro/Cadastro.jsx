2608959fc5399fe2f0a12cd628d59e463d414939
Iago de Souza Lopes <iagodesouza2012@gmail.com>
2024-09-25 08:04:14 -0300

    Criando novos arquivos no projeto

---
+++ src/Pages/Cadastro/Cadastro.jsx
@@ -0,0 +1,16 @@
+/* eslint-disable react/react-in-jsx-scope */
+
+function Cadastro() {
+  return (
+    <div>
+      <h1>Cadastro</h1>
+      <form>
+        <input type="text" placeholder="Email" />
+        <input type="password" placeholder="Password" />
+        <button>Cadastrar</button>
+      </form>
+    </div>
+  );
+}
+
+export default Cadastro;
---
+++ src/Pages/Login/Login.jsx
@@ -0,0 +1,16 @@
+/* eslint-disable react/react-in-jsx-scope */
+
+function Login() {
+  return (
+    <div>
+      <h1>Login</h1>
+      <form>
+        <input type="text" placeholder="Email" />
+        <input type="password" placeholder="Password" />
+        <button>Login</button>
+      </form>
+    </div>
+  );
+}
+
+export default Login;

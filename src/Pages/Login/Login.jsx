bce6f7d8740ad33b98b4622ab2b925756a4df18e
Iago de Souza Lopes <iagodesouza2012@gmail.com>
2024-09-27 17:40:17 -0300

    Estilização da página de Login

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,15 +1,39 @@
 /* eslint-disable react/react-in-jsx-scope */
+import Logo from '/SEKI.svg';
+import './login.css';
 
 function Login() {
   return (
-    <div>
-      <h1>Login</h1>
-      <form>
-        <input type="text" placeholder="Email" />
-        <input type="password" placeholder="Password" />
-        <button>Login</button>
-      </form>
-    </div>
+    <section id="Login">
+      <div className="container-login">
+        <img src={Logo} alt="" />
+        <h1>Acesse sua conta</h1>
+        <form>
+          <div className="input-group">
+            <label>E-mail</label>
+            <input type="text" placeholder="Digite seu E-mail" />
+          </div>
+          <div className="input-group">
+            <label>Senha</label>
+            <input type="password" placeholder="Digite sua senha" />
+          </div>
+          <div className="lembrar-esqueci">
+            <label>
+              <input type="checkbox" className="check-lembrar" />
+              Lembrar de mim
+            </label>
+            <a href="/">Esqueci minha senha</a>
+          </div>
+          <button>Login</button>
+          <div className="dont-have-account">
+            <p>
+              Ainda não tem conta?
+              <a href="/">Cadastre-se</a>
+            </p>
+          </div>
+        </form>
+      </div>
+    </section>
   );
 }
 

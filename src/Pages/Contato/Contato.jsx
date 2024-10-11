3f45e9bdff9cdf2557a41075e883f05746015bf1
Iago de Souza Lopes <iagodesouza2012@gmail.com>
2024-10-10 21:28:52 -0300

    Envio de email pelo emailjs

--- src/Pages/Contato/Contato.jsx
+++ src/Pages/Contato/Contato.jsx
@@ -1,25 +1,45 @@
-/* eslint-disable react/react-in-jsx-scope */
 import './contato.css';
 import ImgContato from '/Contato.svg';
+import { useRef } from 'react';
+import emailjs from '@emailjs/browser';
+import { ToastContainer, toast } from 'react-toastify'; // Importando Toastify
+import 'react-toastify/dist/ReactToastify.css';
 
 function Contato() {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_3v4rnsl', 'template_n42j8gg', form.current, 'vfwarZrM3MChAFVqG').then(
+      () => {
+        toast.success('Mensagem enviada com sucesso!');
+        form.current.reset();
+      },
+      (error) => {
+        console.log(error.text);
+        toast.error('Erro ao enviar mensagem, tente novamente.');
+      }
+    );
+  };
+
   return (
     <section id="Contato">
       <div className="contato">
         <div className="contato-container">
           <h1>Envie uma mensagem</h1>
-          <form>
+          <form ref={form} onSubmit={sendEmail}>
             <label>E-mail *</label>
-            <input type="email" placeholder="Digite seu E-mail" />
+            <input type="email" name="user_email" placeholder="Digite seu E-mail" required />
             <label>Nome</label>
-            <input type="text" placeholder="Digite seu Nome" />
+            <input type="text" name="user_name" placeholder="Digite seu Nome" required />
             <label>Assunto *</label>
-            <input type="text" placeholder="Escreva um Assunto" />
+            <input type="text" name="subject" placeholder="Escreva um Assunto" required />
             <label>Descrição *</label>
-            <textarea placeholder="Mensagem" />
+            <textarea name="message" placeholder="Mensagem" required />
 
             <div className="form-button">
-              <button>Enviar</button>
+              <button type="submit">Enviar</button>
             </div>
           </form>
         </div>
@@ -27,6 +47,17 @@ function Contato() {
           <img src={ImgContato} alt="" />
         </div>
       </div>
+      <ToastContainer
+        position="top-right"
+        autoClose={5000} // Fecha automaticamente após 5 segundos
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+      />
     </section>
   );
 }

c04ac30fa1d2827ca778c23cea19820bf5587346
Iago de Souza Lopes <iagodesouza2012@gmail.com>
2024-11-04 17:02:53 -0300

    Atualizando o layout do atendente, colocando os dados do atendente na tela inicial

--- src/Pages/Atendente/Home/Home.jsx
+++ src/Pages/Atendente/Home/Home.jsx
@@ -1,53 +1,126 @@
 import { useState, useEffect } from 'react';
 import { PieChart, Pie, Cell, Tooltip, Legend } from 'recharts';
 import './home.css';
+import chamadosService from '../../../service/chamadosService';
+import { useNavigate } from 'react-router-dom';
+import usuariosService from '../../../service/usuariosService';
+import empresasService from '../../../service/empresasService';
+import equipamentosService from '../../../service/equipamentosService';
 
 function Home() {
   const [chamadosData, setChamadosData] = useState([]);
   const [usuariosData, setUsuariosData] = useState([]);
   const [empresasData, setEmpresasData] = useState([]);
   const [equipamentosData, setEquipamentosData] = useState([]);
-  // const [chamadosSobResponsabilidade, setChamadosSobResponsabilidade] = useState(4); // Exemplo
+  const [chamadosSobResponsabilidade, setChamadosSobResponsabilidade] = useState(0);
   const [chamadosRecentes, setChamadosRecentes] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
 
   // Exemplo de cores
   const COLORS = ['#ff6384', '#36a2eb', '#ffce56', '#4bc0c0'];
 
+  const navigate = useNavigate();
+  const handleRowClick = (record) => {
+    navigate(`/atendente/chamados/${record.IDdoc}/${record.IDchamado}`);
+  };
+
   useEffect(() => {
     // Aqui você faria as chamadas para buscar os dados reais dos gráficos e dos chamados recentes
-    setChamadosData([
-      { name: 'Não Iniciado', value: 5 },
-      { name: 'Em Andamento', value: 10 },
-      { name: 'Aguardando', value: 3 },
-      { name: 'Concluído', value: 12 }
-    ]);
+    const fetchChamados = async () => {
+      try {
+        // Busca a empresa pelo uidDoc e uidEmpresa
+        const statusData = await chamadosService.findByStatus();
+        const countData = await chamadosService.findCount();
+        const chamadosData = await chamadosService.findByRes();
 
-    setUsuariosData([
-      { name: 'Ativos', value: 15 },
-      { name: 'Inativos', value: 5 }
-    ]);
+        // Converte o objeto em um array
+        const statusArray = Object.keys(statusData).map((key) => ({
+          name: key,
+          value: statusData[key]
+        }));
 
-    setEmpresasData([
-      { name: 'Ativas', value: 4 },
-      { name: 'Inativas', value: 0 }
-    ]);
+        setChamadosData(statusArray);
+        setChamadosSobResponsabilidade(countData);
+        setChamadosRecentes(chamadosData);
+        setLoading(false);
+      } catch (err) {
+        setError(`Erro ao carregar os detalhes da empresa: ${err.message}`);
+        setLoading(false);
+      }
+    };
 
-    setEquipamentosData([
-      { name: 'Ativos', value: 12 },
-      { name: 'Inativos', value: 3 }
-    ]);
+    const fetchUsuariosByStatus = async () => {
+      try {
+        // Busca a empresa pelo uidDoc e uidEmpresa
+        const statusData = await usuariosService.getUsuariosCount();
 
-    // Buscar dados dos chamados recentes do usuário logado
-    const chamadosDoUsuario = [
-      { id: '000001', titulo: 'Erro no sistema', dataAbertura: '2024-10-25', status: 'Em andamento' },
-      { id: '000002', titulo: 'Atualização pendente', dataAbertura: '2024-10-24', status: 'Aguardando' },
-      { id: '000003', titulo: 'Problema de rede', dataAbertura: '2024-10-23', status: 'Não iniciado' },
-      { id: '000004', titulo: 'Backup de dados', dataAbertura: '2024-10-22', status: 'Concluído' },
-      { id: '000005', titulo: 'Erro de login', dataAbertura: '2024-10-21', status: 'Em andamento' }
-    ];
+        // Converte o objeto em um array
+        const statusArray = Object.keys(statusData).map((key) => ({
+          name: key,
+          value: statusData[key]
+        }));
 
-    setChamadosRecentes(chamadosDoUsuario.slice(0, 5)); // Pega os 5 últimos chamados
+        setUsuariosData(statusArray);
+        setLoading(false);
+      } catch (err) {
+        setError(`Erro ao carregar os detalhes da empresa: ${err.message}`);
+        setLoading(false);
+      }
+    };
+
+    const fetchEmpresasByStatus = async () => {
+      try {
+        // Busca a empresa pelo uidDoc e uidEmpresa
+        const statusData = await empresasService.getEmpresasByStatus();
+
+        // Converte o objeto em um array
+        const statusArray = Object.keys(statusData).map((key) => ({
+          name: key,
+          value: statusData[key]
+        }));
+
+        setEmpresasData(statusArray);
+        setLoading(false);
+      } catch (err) {
+        setError(`Erro ao carregar os detalhes da empresa: ${err.message}`);
+        setLoading(false);
+      }
+    };
+
+    const fetchEquipamentosByStatus = async () => {
+      try {
+        // Busca a empresa pelo uidDoc e uidEmpresa
+        const statusData = await equipamentosService.getEquipamentosByStatus();
+
+        // Converte o objeto em um array
+        const statusArray = Object.keys(statusData).map((key) => ({
+          name: key,
+          value: statusData[key]
+        }));
+
+        setEquipamentosData(statusArray);
+        setLoading(false);
+      } catch (err) {
+        setError(`Erro ao carregar os detalhes da empresa: ${err.message}`);
+        setLoading(false);
+      }
+    };
+
+    setChamadosRecentes(); // Pega os 5 últimos chamados
+    fetchChamados();
+    fetchUsuariosByStatus();
+    fetchEmpresasByStatus();
+    fetchEquipamentosByStatus();
   }, []);
+
+  if (loading) {
+    return <div>Carregando...</div>;
+  }
+
+  if (error) {
+    return <div>{error}</div>; // Exibe a mensagem de erro, se houver
+  }
 
   return (
     <div className="home-main">
@@ -65,11 +138,13 @@ function Home() {
               outerRadius={80}
               fill="#8884d8"
               label
+              animationDuration={1000} // Define 1 segundo de animação
             >
               {chamadosData.map((entry, index) => (
                 <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
               ))}
             </Pie>
+
             <Tooltip />
             <Legend />
           </PieChart>
@@ -79,13 +154,13 @@ function Home() {
         <div className="responsabilidade">
           <i className="bi bi-headphones"></i>
           <h3>Chamados sob minha responsabilidade</h3>
-          <div className="responsabilidade-box">10</div>
+          <div className="responsabilidade-box">{chamadosSobResponsabilidade}</div>
         </div>
 
         {/* Gráfico de Empresas */}
         <div className="chart">
           <h3>Empresas</h3>
-          <PieChart width={250} height={250}>
+          <PieChart width={250} height={300}>
             <Pie
               data={empresasData}
               dataKey="value"
@@ -95,6 +170,7 @@ function Home() {
               outerRadius={80}
               fill="#8884d8"
               label
+              animationDuration={1000}
             >
               {empresasData.map((entry, index) => (
                 <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
@@ -108,7 +184,7 @@ function Home() {
         {/* Gráfico de Usuários */}
         <div className="chart">
           <h3>Usuários</h3>
-          <PieChart width={250} height={250}>
+          <PieChart width={250} height={300}>
             <Pie
               data={usuariosData}
               dataKey="value"
@@ -118,11 +194,13 @@ function Home() {
               outerRadius={80}
               fill="#8884d8"
               label
+              animationDuration={1000}
             >
               {usuariosData.map((entry, index) => (
                 <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
               ))}
             </Pie>
+
             <Tooltip />
             <Legend />
           </PieChart>
@@ -131,7 +209,7 @@ function Home() {
         {/* Gráfico de Equipamentos */}
         <div className="chart">
           <h3>Equipamentos</h3>
-          <PieChart width={250} height={250}>
+          <PieChart width={250} height={300}>
             <Pie
               data={equipamentosData}
               dataKey="value"
@@ -140,6 +218,7 @@ function Home() {
               cy="50%"
               outerRadius={80}
               fill="#8884d8"
+              animationDuration={1000}
               label
             >
               {equipamentosData.map((entry, index) => (
@@ -158,15 +237,15 @@ function Home() {
             </a>
           </div>
           <ul className="chamados-list">
-            {chamadosRecentes.map((chamado) => (
-              <li key={chamado.id} className="chamado-item">
+            {chamadosRecentes?.map((chamado) => (
+              <li key={chamado.IDchamado} className="chamado-item" onClick={() => handleRowClick(chamado)}>
                 <div className="chamado-group">
-                  <strong>{chamado.id}</strong>
-                  {chamado.dataAbertura}
+                  <strong>{chamado.IDchamado}</strong>
+                  {chamado.DataCriacao}
                 </div>
                 <div className="chamado-group">
-                  <div>{chamado.titulo}</div>
-                  {chamado.status}
+                  <div>{chamado.Titulo}</div>
+                  {chamado.Status}
                 </div>
               </li>
             ))}

6b4d79c2a0db4a91f4431c5a1e9ac14fdfe7ca08
Iago de Souza Lopes <iagodesouza2012@gmail.com>
2024-11-13 15:12:26 -0300

    Estilizando e estruturando a tela de detalhes do equipamento

--- src/Pages/Equipamentos/Detalhe.jsx
+++ src/Pages/Equipamentos/Detalhe.jsx
@@ -1,12 +1,16 @@
-import { useCallback, useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import equipamentosService from '../../service/equipamentosService';
 import usuariosService from '../../service/usuariosService';
 import empresasService from '../../service/empresasService';
-import { Button, Input, Select, Switch } from 'antd';
+import { Alert, Button, Form, Input, message, Select, Spin, Switch } from 'antd';
+import './detalhe.css';
+import html2canvas from 'html2canvas';
+import { QRCodeCanvas } from 'qrcode.react';
 
 const EquipamentoDetalhes = () => {
   const { id } = useParams(); // Pega os parâmetros da URL
+  const [form] = Form.useForm();
   const [equipamento, setEquipamento] = useState(null);
   const [loading, setLoading] = useState(true); // Estado para controlar o carregamento
   const [error, setError] = useState(null);
@@ -15,240 +19,308 @@ const EquipamentoDetalhes = () => {
   const [usuarios, setUsuarios] = useState([]);
   const [usuarioByUid, setUsuarioByUid] = useState(null);
   const [isChanged, setIsChanged] = useState(false);
-
-  const [marcaTemp, setMarcaTemp] = useState(null);
-  const [modeloTemp, setModeloTemp] = useState(null);
-  const [empresaTemp, setEmpresaTemp] = useState(null);
-  const [setorTemp, setSetorTemp] = useState(null);
-  const [usuarioTemp, setUsuarioTemp] = useState(null);
-  const [statusTemp, setStatusTemp] = useState(null);
-
-  const fetchUsuarioByUid = useCallback(async (uid) => {
-    if (!uid) return;
-
-    try {
-      const usuarioByUidData = await usuariosService.getUsuarioByUid(uid);
-      if (usuarioByUidData) {
-        setUsuarioByUid(usuarioByUidData);
-      } else {
-        setUsuarioByUid([]);
-      }
-    } catch (error) {
-      console.error('Erro ao buscar o usuário:', error);
-      setUsuarioByUid([]);
-    }
-  }, []);
-
-  const fetchSetores = useCallback(async (id) => {
-    if (!id) return;
-
-    try {
-      const setoresData = await empresasService.getSetoresPorEmpresa(id);
-      setSetores(setoresData.map((setor) => ({ value: setor.IDdoc, label: setor.Descricao })));
-    } catch (error) {
-      console.error('Erro ao buscar os setores:', error);
-      setSetores([]);
-    }
-  }, []);
+  const [nomeEmpresa, setNomeEmpresa] = useState('');
+  const [dataFetched, setDataFetched] = useState(false);
 
   useEffect(() => {
+    if (dataFetched) return;
+
     const fetchEquipamento = async () => {
       try {
-        // Busca a empresa pelo uidDoc e uidEmpresa
         const equipamentoData = await equipamentosService.getEquipamentoById(id);
+        setEquipamento(equipamentoData); // Armazena os dados do equipamento no estado
 
-        setEquipamento(equipamentoData); // Armazena os dados da empresa no estado
-        setMarcaTemp(equipamentoData.marca);
-        setModeloTemp(equipamentoData.modelo);
-        setEmpresaTemp(equipamentoData.idEmpresa);
-        setSetorTemp(equipamentoData.idSetor);
-        setUsuarioTemp(equipamentoData.idUsuario);
-        setStatusTemp(equipamentoData.status);
+        const usuarioByUidData = await fetchUsuarioByUid(equipamentoData.quemCriou);
+        setUsuarioByUid(usuarioByUidData);
+
+        const setoresData = await fetchSetores(equipamentoData.idEmpresa);
+        setSetores(setoresData);
+
+        // Depois de carregar todos os dados, marca como "dataFetched"
+        setDataFetched(true);
         setLoading(false); // Define o carregamento como concluído
-
-        fetchUsuarioByUid(equipamentoData.quemCriou);
-        fetchSetores(equipamentoData.idEmpresa);
-      } catch (err) {
-        setError(`Erro ao carregar os detalhes da empresa: ${err.message}`); // Define uma mensagem de erro
+      } catch (error) {
+        setError(`Erro ao carregar os detalhes do equipamento: ${error.message}`); // Define uma mensagem de erro
         setLoading(false); // Define o carregamento como concluído
       }
     };
 
-    fetchEquipamento();
-  }, [id, fetchUsuarioByUid, fetchSetores]);
-
-  useEffect(() => {
     const fetchEmpresas = async () => {
-      const empresasData = await empresasService.getEmpresasDisponiveis();
-      setEmpresas(empresasData.map((empresa) => ({ value: empresa.IDempresa, label: empresa.RazaoSocial })));
+      try {
+        const empresasData = await empresasService.getEmpresasDisponiveis();
+        setEmpresas(
+          empresasData.map((empresa) => ({
+            value: empresa.IDempresa,
+            label: empresa.RazaoSocial
+          }))
+        );
+
+        if (equipamento) {
+          // Após buscar as empresas, encontre a empresa correspondente
+          const empresaCorrespondente = empresasData.find((empresa) => empresa.IDempresa === equipamento.idEmpresa);
+          setNomeEmpresa(empresaCorrespondente ? empresaCorrespondente.RazaoSocial : 'Empresa não encontrada');
+
+          form.setFieldsValue({
+            marca: equipamento.marca,
+            modelo: equipamento.modelo,
+            empresaSelecionada: equipamento.idEmpresa,
+            setorSelecionado: equipamento.idSetor,
+            usuario: equipamento.idUsuario,
+            status: equipamento.status
+          });
+        }
+      } catch (error) {
+        setError(`Erro ao buscar as empresas: ${error.message}`);
+        setLoading(false);
+      }
     };
 
     const fetchUsuarios = async () => {
-      const usuariosData = await usuariosService.getUsuariosPorNivel();
-      setUsuarios(usuariosData.map((usuario) => ({ value: usuario.uid, label: usuario.Nome })));
+      try {
+        const usuariosData = await usuariosService.getUsuariosPorNivel();
+        setUsuarios(
+          usuariosData.map((usuario) => ({
+            value: usuario.uid,
+            label: usuario.Nome
+          }))
+        );
+      } catch (error) {
+        setError(`Erro ao buscar os usuarios: ${error.message}`);
+        setLoading(false);
+      }
     };
 
+    const fetchUsuarioByUid = async (uid) => {
+      try {
+        const usuarioByUidData = await usuariosService.getUsuarioByUid(uid);
+        return usuarioByUidData;
+      } catch (error) {
+        console.error('Erro ao buscar o usuário:', error);
+      }
+    };
+
+    const fetchSetores = async (idEmpresa) => {
+      try {
+        const setoresData = await empresasService.getSetoresPorEmpresa(idEmpresa);
+        return setoresData.map((setor) => ({ value: setor.IDdoc, label: setor.Descricao }));
+      } catch (error) {
+        console.error('Erro ao buscar os setores:', error);
+      }
+    };
+
+    // Chama as funções de busca de dados
+    fetchEquipamento();
     fetchEmpresas();
     fetchUsuarios();
-  }, []);
+  }, [form, id, equipamento, dataFetched]);
 
-  const handleSelectChange = (field, value) => {
-    setIsChanged(true);
-    switch (field) {
-      case 'Marca':
-        setMarcaTemp(value);
-        break;
-      case 'Modelo':
-        setModeloTemp(value);
-        break;
-      case 'IDempresa':
-        setEmpresaTemp(value);
-        break;
-      case 'IDsetor':
-        setSetorTemp(value);
-        break;
-      case 'IDusuario':
-        setUsuarioTemp(value);
-        break;
-      case 'Status':
-        setStatusTemp(value);
-        break;
-      default:
-        break;
-    }
-  };
-
-  const handleSave = async () => {
+  const handleSave = async (values) => {
     try {
       const updatedEquipamento = {
         IDdoc: equipamento.id,
-        Marca: marcaTemp,
-        Modelo: modeloTemp,
-        IDempresa: empresaTemp,
-        IDsetor: setorTemp,
-        IDusuario: usuarioTemp,
-        Status: statusTemp ? 'Ativo' : 'Inativo'
+        Marca: values.marca,
+        Modelo: values.modelo,
+        IDempresa: values.empresaSelecionada,
+        IDsetor: values.setorSelecionado,
+        IDusuario: values.usuario,
+        Status: values.status ? 'Ativo' : 'Inativo'
       };
 
       await equipamentosService.update(updatedEquipamento);
-      alert('Chamado atualizado com sucesso!');
+      message.success('Equipamento atualizado com sucesso!');
       setIsChanged(false);
       // window.location.reload(); // Recarrega a página
-    } catch (error) {
-      console.error('Erro ao atualizar o chamado:', error);
-      alert('Erro ao atualizar o chamado. Tente novamente.');
+    } catch {
+      message.error('Erro ao atualizar o Equipamento. Tente novamente mais tarde.');
     }
   };
 
-  const handleCancel = () => {
-    // Restaura os dados temporários com os valores originais
-    setMarcaTemp(equipamento.marca);
-    setModeloTemp(equipamento.modelo);
-    setEmpresaTemp(equipamento.idEmpresa);
-    setSetorTemp(equipamento.idSetor);
-    setUsuarioTemp(equipamento.idUsuario);
-    setStatusTemp(equipamento.status);
+  const onReset = () => {
+    form.setFieldsValue({
+      marca: equipamento.marca,
+      modelo: equipamento.modelo,
+      empresaSelecionada: equipamento.idEmpresa,
+      setorSelecionado: equipamento.idSetor,
+      usuario: equipamento.idUsuario,
+      status: equipamento.status
+    });
     setIsChanged(false);
   };
 
-  if (loading) {
-    return <div>Carregando...</div>;
-  }
+  const handleSalvarImagem = async () => {
+    const container = document.getElementById('qrcode-container');
+    if (container) {
+      try {
+        const canvas = await html2canvas(container);
+        const dataUrl = canvas.toDataURL('image/png');
 
-  if (error) {
-    return <div>{error}</div>; // Exibe a mensagem de erro, se houver
-  }
-
-  if (!equipamento) {
-    return <div>Equipamento não encontrado.</div>;
-  }
+        const link = document.createElement('a');
+        link.href = dataUrl;
+        link.download = `qrcode_${equipamento.idQrcode}.png`;
+        link.click();
+      } catch (error) {
+        console.error('Erro ao salvar a imagem:', error);
+      }
+    }
+  };
 
   return (
-    <div>
-      <h2>Detalhes do Equipamento</h2>
-
-      <div className="qrcode-container">
-        <div className="info">
-          <label>QRcode</label> {equipamento.idQrcode}
+    <div className="detalhe-equipamento-container">
+      {loading ? (
+        <div className="spin-container">
+          <Spin tip="Carregando detalhes do chamado..." size="large">
+            <div style={{ height: 'auto', width: '200px', background: 'transparent', fontSize: '2rem' }}></div>
+          </Spin>
         </div>
-        <div className="gerar-qrcode">
-          Salvar QRcode?
-          <i
-            className="bi bi-qr-code"
-            disabled={isChanged}
-            style={{ color: isChanged ? 'gray' : 'black', cursor: isChanged ? 'not-allowed' : 'pointer' }}
-          ></i>
-        </div>
-      </div>
+      ) : error ? (
+        <Alert message={error} type="error" showIcon />
+      ) : (
+        <div className="dc-content">
+          <div className="coluna1">
+            <div className="equipamento">
+              <div className="titulo">
+                <label>Equipamento</label>
+              </div>
+              <div className="linha"></div>
+              <div className="info-data">
+                <Form
+                  form={form}
+                  name="control-hooks"
+                  layout="vertical"
+                  style={{
+                    maxWidth: 1000
+                  }}
+                  initialValues={{
+                    remember: true
+                  }}
+                  onFinish={handleSave}
+                  autoComplete="off"
+                  onValuesChange={() => setIsChanged(true)}
+                >
+                  <Form.Item
+                    label="Marca"
+                    name="marca"
+                    rules={[
+                      {
+                        required: true,
+                        message: 'Por favor, insira uma marca!'
+                      }
+                    ]}
+                  >
+                    <Input placeholder="Digite a marca do equipamento" />
+                  </Form.Item>
 
-      <div>
-        <label>Marca:</label>
-        <Input
-          placeholder="Digite a Marca do equipamento"
-          value={marcaTemp}
-          onChange={(e) => handleSelectChange('Marca', e.target.value)}
-        />
-      </div>
+                  <Form.Item
+                    label="Modelo"
+                    name="modelo"
+                    rules={[
+                      {
+                        required: true,
+                        message: 'Por favor, insira um Modelo!'
+                      }
+                    ]}
+                  >
+                    <Input placeholder="Digite o modelo do equipamento" />
+                  </Form.Item>
 
-      <div>
-        <label>Modelo:</label>
-        <Input
-          placeholder="Digite o Modelo do equipamento"
-          value={modeloTemp}
-          onChange={(e) => handleSelectChange('Modelo', e.target.value)}
-        />
-      </div>
+                  <Form.Item
+                    label="Empresa"
+                    name="empresaSelecionada"
+                    rules={[
+                      {
+                        required: true,
+                        message: 'Por favor, selecione uma empresa!'
+                      }
+                    ]}
+                  >
+                    <Select
+                      showSearch
+                      placeholder="Selecione uma empresa"
+                      optionFilterProp="label"
+                      options={empresas}
+                    />
+                  </Form.Item>
 
-      <div>
-        <label>Empresa:</label>
-        <Select
-          showSearch
-          placeholder="Selecione uma empresa"
-          optionFilterProp="label"
-          value={empresaTemp}
-          onChange={(value) => handleSelectChange('IDempresa', value)}
-          options={empresas}
-        />
-      </div>
+                  <Form.Item
+                    label="Setor"
+                    name="setorSelecionado"
+                    rules={[
+                      {
+                        required: true,
+                        message: 'Por favor, selecione um Nível de acesso!'
+                      }
+                    ]}
+                  >
+                    <Select
+                      showSearch
+                      placeholder="Selecione um nível de acesso"
+                      optionFilterProp="label"
+                      options={setores}
+                    />
+                  </Form.Item>
 
-      <div>
-        <label>Setor:</label>
-        <Select
-          showSearch
-          placeholder="Selecione um setor"
-          optionFilterProp="label"
-          value={setorTemp}
-          onChange={(value) => handleSelectChange('IDsetor', value)}
-          options={setores}
-        />
-      </div>
+                  <Form.Item label="Usuario" name="usuario">
+                    <Select
+                      showSearch
+                      placeholder="Selecione um nível de acesso"
+                      optionFilterProp="label"
+                      options={usuarios}
+                    />
+                  </Form.Item>
 
-      <div>
-        <label>Usuario:</label>
-        {equipamento.idUsuario ? null : <label> Não pertence a nenhum usuário. Deseja selecionar?</label>}
-        <Select
-          showSearch
-          placeholder="Selecione um usuario"
-          optionFilterProp="label"
-          value={usuarioTemp}
-          onChange={(value) => handleSelectChange('IDusuario', value)}
-          options={usuarios}
-        />
-      </div>
+                  <Form.Item
+                    label="Status"
+                    name="status"
+                    rules={[
+                      {
+                        required: true,
+                        message: 'Por favor, selecione um usuario!'
+                      }
+                    ]}
+                  >
+                    <Switch />
+                  </Form.Item>
 
-      <p>Quem criou? {usuarioByUid ? usuarioByUid.nome : 'Usuário não encontrado'}</p>
+                  <div className="botoes">
+                    <Button type="primary" htmlType="submit" disabled={!isChanged}>
+                      Salvar
+                    </Button>
+                    <Button onClick={onReset} disabled={!isChanged}>
+                      Cancelar
+                    </Button>
+                  </div>
+                </Form>
+              </div>
+            </div>
+          </div>
 
-      <div>
-        <label>Status:</label>
-        <Switch checked={statusTemp} onChange={(checked) => handleSelectChange('Status', checked)} />
-      </div>
+          <div className="coluna2">
+            <div className="detalhe">
+              <div className="title">
+                <label>Informações</label>
+              </div>
+              <div className="linha"></div>
+              <div>
+                <div className="qrcode-container">
+                  <div className="info">
+                    <label>QRcode</label>
+                  </div>
+                  <div className="gerar-qrcode">
+                    {equipamento.idQrcode}
+                    <QRCodeCanvas value={equipamento.idQrcode} size={200} />
+                    <label>{nomeEmpresa}</label>
+                    <Button type="primary" onClick={handleSalvarImagem}>
+                      Salvar como PNG
+                    </Button>
+                  </div>
+                </div>
+              </div>
 
-      {isChanged && (
-        <div>
-          <Button type="primary" onClick={handleSave}>
-            Salvar
-          </Button>
-          <Button onClick={handleCancel}>Cancelar</Button>
+              <div className="info">
+                <label>Criado por</label> {usuarioByUid ? usuarioByUid.nome : 'Usuário não encontrado'}
+              </div>
+            </div>
+          </div>
         </div>
       )}
     </div>
